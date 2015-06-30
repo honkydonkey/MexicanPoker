5ce203baf4ac10e5a91e0cdf129677cdee8ff973
Arata Honda <havoc0214@yahoo.co.jp>
2015-06-30 20:57:59 +0900

    Create PokerMain.java

---
+++ PokerMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokermain;
+
+/**
+ *
+ * @author f1110801062
+ */
+public class PokerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Poker p = new Poker();
+        // TODO code application logic here
+    }
+}
